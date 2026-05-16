bebb0325602e7ed77d13cf008ad40a903b014beb
gilang1302 <113487534+gilang1302@users.noreply.github.com>
2026-05-16 21:42:51 +0700

    Update owner.blade.php

--- resources/views/owner.blade.php
+++ resources/views/owner.blade.php
@@ -1,5 +1,3 @@
-<h1>Dashboard Owner</h1>
-
-<p>
-    Monitoring laporan dan statistik bisnis
-</p>
+<h2>Total Reservasi Hari Ini: 12</h2>
+<h2>Total Pendapatan: Rp850.000</h2>
+<h2>Meja Aktif: 5</h2>
