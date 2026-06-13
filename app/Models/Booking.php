e5b7cb1c053bde7f286a2b040c20e1896d942134
gilang1302 <113487534+gilang1302@users.noreply.github.com>
2026-06-14 03:45:23 +0700

    progress

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -9,4 +9,12 @@ class Booking extends Model
     protected $table = 'booking';
     protected $keyType = 'string';
     public $incrementing = false;
+    
+    protected $fillable = [
+    'customer_name',
+    'table_id',
+    'booking_date',
+    'status'
+    ];
 }
+
