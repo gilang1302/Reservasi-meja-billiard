330d506ecb8c218e51c61e7043e5cd07cd83a0f4
gilang1302 <113487534+gilang1302@users.noreply.github.com>
2026-06-14 03:46:03 +0700

    progress

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -9,4 +9,9 @@ class Inventory extends Model
     protected $table = 'Inventory';
     protected $keyType = 'string';
     public $incrementing = false;
+
+    protected $fillable = [
+    'item_name',
+    'quantity'
+    ];
 }
